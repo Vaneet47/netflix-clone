5209035123488ec5355f15abee5076ede892f362
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-04-04 17:26:10 +0530

    Register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './app.scss';
 import Home from './pages/home/Home.jsx';
 import Watch from './pages/watch/Watch.jsx';
+import Register from './pages/register/Register.jsx';
 
 function App() {
-  return <Watch />;
+  return <Register />;
 }
 
 export default App;
