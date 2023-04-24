6687ac7f48bc702f961a8b3ee20a2cbb9c53bce2
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-04-25 02:32:44 +0530

    Admin topbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Topbar></Topbar>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
