377636273c6b1dde7e7d24fc5d86a723ffe78df9
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-04-25 08:59:05 +0530

    sidebar and other pages layout

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+import './app.css';
 
 function App() {
   return (
     <div>
-      <Topbar></Topbar>
+      <Topbar />
+      <div className='container'>
+        <Sidebar />
+        <div className='others'>Other Pages</div>
+      </div>
     </div>
   );
 }
