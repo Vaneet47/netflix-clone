5d3e848cb4721142990a004f2da518dbad99c813
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-04-26 21:35:56 +0530

    Recharts component

---
+++ admin/src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: 'Jan',
+    'Active User': 4000,
+  },
+  {
+    name: 'Feb',
+    'Active User': 3000,
+  },
+  {
+    name: 'Mar',
+    'Active User': 2000,
+  },
+  {
+    name: 'Apr',
+    'Active User': 1550,
+  },
+  {
+    name: 'May',
+    'Active User': 8005,
+  },
+  {
+    name: 'Jun',
+    'Active User': 4938,
+  },
+  {
+    name: 'Jul',
+    'Active User': 5858,
+  },
+  {
+    name: 'Aug',
+    'Active User': 1389,
+  },
+  {
+    name: 'Sep',
+    'Active User': 3892,
+  },
+  {
+    name: 'Oct',
+    'Active User': 13998,
+  },
+  {
+    name: 'Nov',
+    'Active User': 4883,
+  },
+  {
+    name: 'Dec',
+    'Active User': 4000,
+  },
+];
