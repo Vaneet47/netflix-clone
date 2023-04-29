f552a1a379a6d8aa756c0173bc5cbb8f4c82ad74
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-04-30 00:07:03 +0530

    product list

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,6 +12,7 @@ import {
 } from 'react-router-dom';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
 
 function App() {
   return (
@@ -30,6 +31,15 @@ function App() {
             <User />
           </Route>
           <Route path='/newUser'>
+            <NewUser />
+          </Route>
+          <Route path='/products'>
+            <ProductList />
+          </Route>
+          <Route path='/product/:productId'>
+            <User />
+          </Route>
+          <Route path='/newProduct'>
             <NewUser />
           </Route>
         </Switch>
---
+++ admin/src/pages/productList/ProductList.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './productList.css';
+import { DataGrid } from '@mui/x-data-grid';
+import { productListRows } from '../../dummyData';
+import { Link } from 'react-router-dom';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+
+function ProductList() {
+  const [data, setData] = useState(productListRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'name',
+      headerName: 'Product',
+      width: 160,
+      renderCell: (params) => {
+        debugger;
+        return (
+          <div className='productListProduct'>
+            <img className='productListImg' src={params.row.img} alt='' />
+            {params.row.name}
+          </div>
+        );
+      },
+    },
+    { field: 'stock', headerName: 'Stock', width: 180 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 160,
+    },
+    {
+      field: 'price',
+      headerName: 'Price',
+      width: 180,
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={'/product/' + params.row.id}>
+              <button className='productListEdit'>Edit</button>
+            </Link>
+            <DeleteOutlineIcon
+              className='productListDelete'
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className='productList'>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 9,
+            },
+          },
+        }}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </div>
+  );
+}
+
+export default ProductList;
