f672966a9680e95f2848f615ec48642ba2e5bf9f
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-05-21 02:28:59 +0530

    AuthContext + login page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -15,14 +15,18 @@ import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
 import Product from './pages/product/Product';
 import NewProduct from './pages/newProduct/NewProduct';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <Router>
-      <Topbar />
-      <div className='container'>
-        <Sidebar />
-        <Switch>
+      <Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Topbar />
+        <div className='container'>
+          <Sidebar />
           <Route exact path='/'>
             <Home />
           </Route>
@@ -47,8 +51,8 @@ function App() {
           <Route path='/newProduct'>
             <NewProduct />
           </Route>
-        </Switch>
-      </div>
+        </div>
+      </Switch>
     </Router>
   );
 }
