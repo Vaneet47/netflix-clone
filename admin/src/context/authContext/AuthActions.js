7127842b8b6d82aee430485840ff0e18e2a7dcd3
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-05-21 05:10:46 +0530

    logout action, reducer

--- admin/src/context/authContext/AuthActions.js
+++ admin/src/context/authContext/AuthActions.js
@@ -8,3 +8,15 @@ export const loginSuccess = (user) => ({
 export const loginFailure = () => ({
   type: 'LOGIN_FAILURE',
 });
+
+// logout
+export const logoutStart = () => ({
+  type: 'LOGOUT',
+});
+// export const logoutSuccess = (user) => ({
+//   type: 'LOGOUT_SUCCESS',
+//   payload: user,
+// });
+// export const logoutFailure = () => ({
+//   type: 'LOGOUT_FAILURE',
+// });
